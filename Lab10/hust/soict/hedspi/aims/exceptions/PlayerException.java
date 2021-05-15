ebfc7da33a5fa47d1753b969b64b13f48795f02c
nguyenhuongvi1712 <huongvinguyen171200@gmail.com>
2021-05-15 21:42:26 +0700

    Lab10

--- Lab10/hust/soict/hedspi/aims/exceptions/PlayerException.java
+++ Lab10/hust/soict/hedspi/aims/exceptions/PlayerException.java
@@ -1,7 +1,27 @@
 package hust.soict.hedspi.aims.exceptions;
 
 public class PlayerException extends Exception{
-    public PlayerException(String e){
-        super(e);
+    public PlayerException() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public PlayerException(String arg0) {
+        super(arg0);
+        // TODO Auto-generated constructor stub
+    }
+
+    public PlayerException(Throwable arg0) {
+        super(arg0);
+        // TODO Auto-generated constructor stub
+    }
+
+    public PlayerException(String arg0, Throwable arg1) {
+        super(arg0, arg1);
+        // TODO Auto-generated constructor stub
+    }
+
+    public PlayerException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+        super(arg0, arg1, arg2, arg3);
+        // TODO Auto-generated constructor stub
     }
 }
